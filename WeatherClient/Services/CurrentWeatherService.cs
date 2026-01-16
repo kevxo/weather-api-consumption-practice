07c60478793b6b59fcd9675d2ea880b5d1eb6b75
Kevin Cuadros <kev040798@gmail.com>
2026-01-16 14:53:33 -0500

    Deserialize API response

--- WeatherClient/Services/CurrentWeatherService.cs
+++ WeatherClient/Services/CurrentWeatherService.cs
@@ -1,6 +1,7 @@
 using System.Net.Http.Json;
 using Microsoft.Extensions.Configuration;
 using WeatherClient.Models.CurrentWeather;
+using Newtonsoft.Json;
 
 namespace WeatherClient.Services.CurrentWeatherService;
 
@@ -22,15 +23,28 @@ class CurrentWeatherService
 
         var url = $"{baseUrl}?q={q}&key={apiKey}";
 
-        var response = await _httpClient.GetAsync(url);
+        var request = await _httpClient.GetAsync(url);
 
-        if (!response.IsSuccessStatusCode)
+        if (!request.IsSuccessStatusCode)
         {
-            Console.WriteLine($"Error: {response.StatusCode}");
+            Console.WriteLine($"Error: {request.StatusCode}");
             return null;
         }
 
-        Console.WriteLine(response.Content);
-        return await response.Content.ReadFromJsonAsync<CurrentWeatherResponse>();        
+        var response = await request.Content.ReadAsStringAsync();
+
+        CurrentWeatherResponse currentWeather = new CurrentWeatherResponse();
+
+        var jsonResponse = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(response);
+       
+        currentWeather.City = jsonResponse?["location"]?["name"]?.ToString() ?? string.Empty;
+        currentWeather.Country = jsonResponse?["location"]?["country"]?.ToString() ?? string.Empty;
+        currentWeather.State = jsonResponse?["location"]?["region"]?.ToString() ?? string.Empty;
+        currentWeather.LocalTime = jsonResponse?["location"]?["localtime"]?.ToString() ?? string.Empty;
+        currentWeather.TimeZone = jsonResponse?["location"]?["tz_id"]?.ToString() ?? string.Empty;
+        currentWeather.Celsius = jsonResponse?["current"]?["temp_c"]?.ToString() ?? string.Empty;
+        currentWeather.Fahrenheit = jsonResponse?["current"]?["temp_f"]?.ToString() ?? string.Empty;
+
+        return currentWeather;    
     }
 }
